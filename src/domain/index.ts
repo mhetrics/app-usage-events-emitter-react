c89b41c1daed103c4f8fdb6ef3fc91fcc3b6114c
Ulad Kasach <uladkasach@gmail.com>
2023-07-16 03:46:57 -0700

    fix(pkg): export domain objects

---
+++ src/domain/index.ts
@@ -0,0 +1,7 @@
+export * from './AppUsageApplication';
+export * from './AppUsageBrowser';
+export * from './AppUsageDevice';
+export * from './AppUsageEvent';
+export * from './AppUsageNetwork';
+export * from './AppUsageSession';
+export * from './AsyncTaskEmitToRemote';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { AppUsageEventProvider } from './logic/AppUsageEventProvider';
+export * from './domain';
